7a8ec5aafa41f733f826fc651edae1f3dc7ca3d7
gfplk <gfplk_admin@163.com>
2017-06-08 19:37:06 +0800

    xpath和re解析封装

--- parser.py
+++ parser.py
@@ -1,4 +1,14 @@
 from scrapy.selector import Selector
+import re
 
-class Parser(Selector):
-    pass
+class XpathParser(Selector):
+    def __init__(self, text):
+        super().__init__(text=text)
+
+class ReParser(object):
+    def __init__(self, re_text):
+        self.match = re.compile(re_text)
+
+    def compute(self, text):
+        m = self.match.search(text)
+        return m.group() if m else None
