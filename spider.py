e768693c18d75169dc831b3b7d66c04254bb54f6
gfplk <gfplk_admin@163.com>
2017-06-08 19:37:32 +0800

    虫子http请求封装

--- spider.py
+++ spider.py
@@ -1,6 +1,10 @@
-class Spider:
-    def get(url, playload, cookies, headers, timeout, proxies):
-        pass
+import requests
 
-    def post(url, playload, form, cookies, headers, timeout, proxies):
-        pass
+class Spider(object):
+    def get(url, playload={}, cookies={}, headers={}, timeout=30, proxies={}):
+        return requests.get(url, playload=playload, cookies=cookies, 
+            headers=headers, timeout=timeout, proxies=proxies)
+
+    def post(url, playload={}, cookies={}, headers={}, timeout=30, proxies={}, verify=False):
+        return requests.post(url, playload=playload, cookies=cookies, 
+            headers=headers, timeout=timeout, proxies=proxies, verify=verify)
