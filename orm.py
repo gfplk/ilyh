d8a261a115089429d67a2b4694f068870340e0e7
gfplk <gfplk_admin@163.com>
2017-06-19 01:47:28 +0800

    orm框架

--- orm.py
+++ orm.py
@@ -1,10 +1,13 @@
 from threading import Thread
 from pymysql import Connection
 
-__POOL = []
+__POOL = []  # 数据库连接池
 
 
 def create_pool(size, **kwargs):
+    '''
+    创建一个连接池
+    '''   
     global __POOL
     for i in range(size):
         __POOL.append(Connection(kwargs))
