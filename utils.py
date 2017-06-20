0664c178655fdc8cbbb1c4f9c76a6116065f7e2d
gfplk <gfplk_admin@163.com>
2017-06-20 23:47:11 +0800

    lowerCase

--- utils.py
+++ utils.py
@@ -2,7 +2,10 @@ import random
 
 
 def rand_user_agent():
-    USER_AGENT_LIST = [
+    '''
+    随机请求头
+    '''
+    uas = [
         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
         "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
@@ -22,4 +25,4 @@ def rand_user_agent():
         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
         "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
     ]
-    return USER_AGENT_LIST[random.randint(0, len(USER_AGENT_LIST) - 1)]
+    return uas[random.randint(0, len(uas) - 1)]
