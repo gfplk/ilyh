f8109fa3d4a12018e62011a0a3f64fd095308f3d
gfplk <gfplk_admin@163.com>
2017-06-20 23:49:38 +0800

    移动以前删掉的test.py，并为其改名为队列方面的测试

---
+++ tests/message_queue_test.py
@@ -0,0 +1,38 @@
+from message_queue import *
+import time
+
+
+class MessageQueueTest:
+    def test_0_import(self):
+        from message_queue import _Task
+        print(_Task)
+
+    def test_1_send(self):
+        def func(producter):
+            func_args = ('Love Wang Xue', 'Love someone forever')
+            print(func_args)
+            for i in func_args:
+                producter.sendTask(i)
+
+        producter = Producter('admin', '123456', 'db1.ichunt.com',
+                              'love', True)
+        producter.produce(func)
+
+    def test_2_customer(self):
+        def func(customer, body):
+            print('Received', body)
+            customer.storeData(body)
+            time.sleep(10)
+
+        customer = Customer('admin', '123456', 'ilyh.club', 5, True, False,
+                            'love', 'love someone forever')
+        customer.servForever(func)
+
+
+if __name__ == '__main__':
+    mqt = MessageQueueTest()
+    mqt.test_0_import()
+    '''
+    mqt.test_1_send()
+    mqt.test_2_customer()
+    '''
