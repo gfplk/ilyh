14b388b90e226e447fafcc0af261f0d01eab5429
gfplk <gfplk_admin@163.com>
2017-06-20 23:46:41 +0800

    comment

--- parser.py
+++ parser.py
@@ -1,10 +1,14 @@
 from scrapy.selector import Selector
 import re
 
+# xpath 解析器
 XpathParser = Selector
 
 
 class ReParser(object):
+    '''
+    正则表达式解析封装
+    '''
     def __init__(self, re_text):
         self.match = re.compile(re_text)
 
