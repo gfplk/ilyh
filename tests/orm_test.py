eee548163713fc2ff7e2b7ab492b30a95d414d30
gfplk <gfplk_admin@163.com>
2017-06-21 22:03:20 +0800

    测试发现，innodb插入操作确实需要事物处理的，而myiasm确实是不需要事物处理的，因为myiasm插入数据库后，在数据库中立刻就可以查出来，而innodb如果插入后，没有进行事物操作，就会在数据库中没有记录，而myiasm的主键是延迟更新的，所以当你插入myiasm的数据库表后，想获取是获取不到的，由于inoodb支持事物，插入速度和myiasm几乎没有区别

--- tests/orm_test.py
+++ tests/orm_test.py
@@ -3,9 +3,11 @@ from ilyh.orm import create_pool, close_pool, __POOL, select, execute
 import logging
 import pprint
 import sys
-logging.basicConfig(level=logging.DEBUG)
+import collections
+import time
+#logging.basicConfig(level=logging.DEBUG)
 
-KWARGS = {
+DB_JOB_KWARGS = {
     'user': 'gfplk',
     'password': '51job_fuck',
     'database': 'db_job',
@@ -13,10 +15,18 @@ KWARGS = {
     'host': 'ilyh.club'
 }
 
+DB_TEST_KWARGS = {
+    'user': 'gfplk',
+    'password': '51job_fuck',
+    'database': 'db_test',
+    'charset': 'utf8',
+    'host': 'ilyh.club'
+}
+
 
 def pool():
     global KWARGS
-    create_pool(10, **KWARGS)
+    create_pool(10, **DB_JOB_KWARGS)
     for i in __POOL:
         print(i)
     close_pool()
@@ -24,7 +34,7 @@ def pool():
 
 def sel():
     global KWARGS
-    create_pool(1, **KWARGS)
+    create_pool(1, **DB_JOB_KWARGS)
     sql = 'select * from tb_job'
     size = 2
     args = None
@@ -32,17 +42,43 @@ def sel():
     def func():
         pprint.pprint(select(sql, args, size))
 
-    tasks = [Thread(target=func) for i in range(10)]
+    tasks = [Thread(target=func) for i in range(1000)]
     for task in tasks:
         task.start()
 
     for task in tasks:
         task.join()
 
+def exe():
+    global KWARGS
+    create_pool(10, **DB_TEST_KWARGS)
+    sql = 'insert into tb_test(a, b, c, d) values(%s, %s, %s, %s)'
+    test = {
+        'a': 'a',
+        'b': 'b',
+        'c': 'c',
+        'd': 'd',
+    }
+    args = (test['a'], test['b'], test['c'], test['d'])
+    def func():
+        pprint.pprint(execute(sql, args))
+    tasks = [Thread(target=func) for i in range(1000)]
+    now = time.time()
+    for task in tasks:
+        task.start()
+    for task in tasks:
+        task.join()
+    now = time.time() - now
+    print('spent %r seconds.' % now)
+
+    for i in __POOL:
+        print(i['is_used'])
+
 
 if __name__ == '__main__':
     try:
-        # pool()
-        sel()
+        #pool()
+        #sel()
+        exe()
     except KeyboardInterrupt as e:
         close_pool()
