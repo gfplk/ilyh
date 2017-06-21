7cdb87733f80754d79f995ec9bd24d2b0d33587a
gfplk <gfplk_admin@163.com>
2017-06-21 23:35:41 +0800

    删除这个垃圾代码

--- tests/thread_lock.py
+++
@@ -1,9 +0,0 @@
-#coding=utf-8
-import time,threading
-lock=threading.Lock()
-def fun(str):
-    print(time.strftime('%H:%M:%S'))
-    time.sleep(5)
-
-for i in range(10):
-    t = threading.Thread(target=fun, args=('hi',)).start()
