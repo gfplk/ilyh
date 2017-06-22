f06c2d6947ec910eb880435a4a411f927f109ace
gfplk <gfplk_admin@163.com>
2017-06-23 02:12:07 +0800

    fix bug 不能引入私有模块class的异常

--- tests/message_queue_test.py
+++ tests/message_queue_test.py
@@ -1,18 +1,22 @@
-from message_queue import *
+from ilyh.message_queue import *
 import time
 
 
 class MessageQueueTest:
     def test_0_import(self):
-        from message_queue import _Task
-        print(_Task)
+        try:
+            from message_queue import _Task
+            print(_Task)
+        except:
+            pass
+        pass
 
     def test_1_send(self):
         def func(producter):
             func_args = ('Love Wang Xue', 'Love someone forever')
             print(func_args)
             for i in func_args:
-                producter.sendTask(i)
+                producter.send_task(i)
 
         producter = Producter('admin', '123456', 'db1.ichunt.com',
                               'love', True)
@@ -21,18 +25,16 @@ class MessageQueueTest:
     def test_2_customer(self):
         def func(customer, body):
             print('Received', body)
-            customer.storeData(body)
+            customer.store_data(body)
             time.sleep(10)
 
         customer = Customer('admin', '123456', 'ilyh.club', 5, True, False,
                             'love', 'love someone forever')
-        customer.servForever(func)
+        customer.serv_forever(func)
 
 
 if __name__ == '__main__':
     mqt = MessageQueueTest()
     mqt.test_0_import()
-    '''
     mqt.test_1_send()
     mqt.test_2_customer()
-    '''
